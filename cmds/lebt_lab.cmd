baa34441e238c3c8505ec44832884d23674bc4d6
antonisimelio <antonisimelio@ci0021274.esss.lu.se>
2019-12-18 16:38:54 +0100

    Test in CS Lab

--- cmds/lebt_lab.cmd
+++ cmds/lebt_lab.cmd
@@ -1,21 +1,39 @@
-require asyn,4.31+
-require streamdevice, 2.7.7
-require sorensenxg125120, 0.5+
-require sorensensga30x501d,0.5+
-require iocStats,3.1+
+#require asyn,4.31+
+#require streamdevice, 2.7.7
+#require sorensenxg125120, 0.5+
+#require sorensensga30x501d,0.5+
+#require iocStats,3.1+
 
-epicsEnvSet("SAVEFILE_DIR", "/opt/shared/isrc-lebt/savePath")
+## NEW
+require stream,2.8.8
+require lebt,master
+
+
+
+#epicsEnvSet("SAVEFILE_DIR", "/opt/shared/isrc-lebt/savePath")
+
+
 
 ## SOLENOIDS
-requireSnippet(sorensen30x501d.cmd)
+#requireSnippet(sorensen30x501d.cmd)
 
 
 ## STEERERS
-requireSnippet(sorensenXG125120.cmd)
+#requireSnippet(sorensenXG125120.cmd)
 
 
 ## iocStats
-dbLoadTemplate(iocAdminSoft.substitutions,IOC="ISrc-010:Ctrl-IOC-Lebt")
+#dbLoadTemplate(iocAdminSoft.substitutions,IOC="ISrc-010:Ctrl-IOC-Lebt")
+
+
+## NEW
+############################################################
+######## TEST: FAST-PS 2020-400 [Power Supply] #############
+############################################################
+#drvAsynIPPortConfigure("HVPS", "172.16.60.57:2101")
+dbLoadRecords("MagneticField.db")
+
+
 
 
 iocInit
@@ -24,8 +42,6 @@ iocInit
 ###########################################################
 ### Operation Parameters from ICSHWI-1265
 ###########################################################
-
-
 dbpf LEBT-010:PwrC-PSCH-01:VolS.DRVH 12.5
 dbpf LEBT-010:PwrC-PSCH-01:VolS.ADEL 0.5
 dbpf LEBT-010:PwrC-PSCH-01:VolS.PREC 1
@@ -172,3 +188,4 @@ dbpf LEBT-010:PwrC-SolPS-02:CurR.HHSV "MAJOR"
 dbpf LEBT-010:PwrC-SolPS-02:CurR.HSV "MINOR"
 dbpf LEBT-010:PwrC-SolPS-02:FldS.EGU "mT"
 dbpf LEBT-010:PwrC-SolPS-02:FldRB.EGU "mT"
+
